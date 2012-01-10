e66ccf09135ff0caa42a3f9da07fcb9f4a88e430
Mike Ball <mikedball@gmail.com>
2012-01-10 12:39:12 -0500

    Added spec directory, example test, and passing test for Integers.

---
+++ spec/exercise_spec.rb
@@ -0,0 +1,18 @@
+# Example:
+describe "Strings" do
+  context "when calling strip" do
+    it "should remove all white space fromt he beginning and end of the string" do
+    
+      " foo ".strip.should eq "foo" 
+      
+    end
+  end
+end
+
+describe "Integers" do
+  context "when calling even" do
+    it "should return true if an integer is even" do
+      2.even?.should eq true 
+    end
+  end
+end
